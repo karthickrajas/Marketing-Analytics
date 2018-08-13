c96dcf2353119f49404d57dcb1a38671e7cbb811
karthickrajas <karthick11b36@gmail.com>
2018-08-13 20:23:12 +0530

    check

--- update.bat
+++ update.bat
@@ -2,6 +2,9 @@
 set /P comment=comment? 
 git status
 git add --all
+echo "added all items"
 git commit -am %comment%
 git pull origin master
+echo "pulled all the records"
 git push origin master
+echo "pushes succesfully"
